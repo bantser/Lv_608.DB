156687956182470950e4bdd136a416a7c51cc1fd
Roksolana <60979763+roksolanaabr@users.noreply.github.com>
2021-06-24 13:18:22 +0300

    Update Shop-LV-608.sql
    Added Products table

--- Shop-LV-608.sql
+++ Shop-LV-608.sql
@@ -1,3 +1,20 @@
+CREATE TABLE Products (
+
+	product_id integer PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	productName nvarchar (30) NOT NULL,
+	quantityInStock integer NOT NULL,
+	salesPrice numeric NOT NULL,
+	typeProduct nvarchar (30) NOT NULL,
+	gender nvarchar (1) NOT NULL,
+	season nvarchar (10) NOT NULL,
+	size nvarchar (2) NOT NULL,
+	brand_id integer,
+
+	FOREIGN KEY (brand_id) REFERENCES Brands (brand_id),
+
+	CHECK (quantityInStock>=0 AND salesPrice>=0)
+
+)
 
 CREATE TABLE Customers(
 	CustomerID INT PRIMARY KEY IDENTITY(1, 1) NOT NULL
