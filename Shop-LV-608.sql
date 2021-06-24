9b09e8d847f82a77fb0bc6b33b40ba437ab0cc16
RomanLyn <iromanlyn@gmail.com>
2021-06-24 15:42:32 +0300

    Update Shop-LV-608.sql

--- Shop-LV-608.sql
+++ Shop-LV-608.sql
@@ -1,3 +1,51 @@
+
+
+
+USE [Test_608DB]
+GO
+
+
+
+
+/****** Object:  Table [dbo].[Table_1]    Script Date: 6/24/2021 2:51:26 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE TABLE Brands(
+       BrandID int NOT NULL PRIMARY KEY IDENTITY(1,1)
+      ,BrandName nvarchar(50) NULL 
+      ,CountryOfManufacture nvarchar(50)NULL)
+
+CREATE TABLE Suppliers(
+     SupplID int NOT NULL PRIMARY KEY IDENTITY(1,1)
+    ,AddressID int NOT NULL
+    ,ContactName nvarchar(50) NULL
+    ,Phone int NULL
+    ,Email nvarchar(50) NULL,
+    HomePage nvarchar(50))
+
+	CREATE TABLE BrandSuppliers(
+   BrandID int NOT NULL FOREIGN KEY REFERENCES Brands(BrandID)
+  ,SupplID int NOT NULL FOREIGN KEY REFERENCES Suppliers(SupplID)
+  PRIMARY KEY(BrandID,SupplID)
+)
+
+
+CREATE TABLE Addresses
+(
+	AddressID int PRIMARY KEY IDENTITY (1, 1),
+	AddressType int NULL,
+	Apartment varchar(10) NULL,
+	City varchar(30)  NULL,
+	Region varchar(25) NULL,
+	PostalCode int NULL
+)
+
+
 CREATE TABLE Products (
 
 	ProductID integer PRIMARY KEY IDENTITY (1, 1) NOT NULL
@@ -13,6 +61,7 @@ CREATE TABLE Products (
 	
 )
 
+
 CREATE TABLE Customers(
 	CustomerID int PRIMARY KEY IDENTITY(1, 1) NOT NULL
 	, FirstName nvarchar (30) NULL
@@ -21,75 +70,21 @@ CREATE TABLE Customers(
 	, DateOfBirth date NULL
 	, Phone nchar (13) NULL
 	, Email nvarchar (50) NULL
-	, AdressID int FOREIGN KEY (Adress_id) REFERENCES Adress (Adress_id)
+	, AddressID int FOREIGN KEY (AddressID) REFERENCES Addresses (AddressID)
 )
 
-CREATE TABLE Returns(
-       Return_ID INT PRIMARY KEY IDENTITY NOT NULL
-       , OrderDetail_ID int NULL
-       , ReturnQuantity int NULL
-       , ReturnDate date NULL
-       , ReturnReason nvarchar(50) NULL
-       , ReturnStatus nvarchar(30) NULL
-       FOREIGN KEY (OrderDetailId) REFERENCES OrderDetails (OrderDetailId)
+
+
+CREATE TABLE Deliveries(
+       DeliveryID INT PRIMARY KEY IDENTITY NOT NULL
+       , AddressID INT  FOREIGN KEY (AddressID) REFERENCES Addresses (AddressID)
+       , CompanyName NVARCHAR(50) NULL
+       , DepartureDate DATE NULL
 )
-
-CREATE TABLE Brands(
-       BrandID int NOT NULL PRIMARY KEY IDENTITY(1,1)
-      ,BrandName nvarchar(50) NULL 
-      ,CountryOfManufacture nvarchar(50)NULL
-
-);
-
-
-CREATE TABLE Suppliers(
-     SupplID int NOT NULL PRIMARY KEY IDENTITY(1,1)
-    ,AdressID int NOT NULL
-    ,ContactName nvarchar(50) NULL
-    ,Phone int NULL
-    ,Email nvarchar(50) NULL,
-    HomePage nvarchar(50)
-
-);
-
-
-CREATE TABLE BrandSuppliers(
-   BrandID int NOT NULL FOREIGN KEY REFERENCES Brands(BrandID)
-  ,SupplID int NOT NULL FOREIGN KEY REFERENCES Suppliers(SupplID)
-  PRIMARY KEY(BrandID,SupplID)
-);
-
-
-
-CREATE TABLE PurchaseOrders(
-	PurchaseOrderID INT PRIMARY KEY IDENTITY NOT NULL,
-	SuplierID INT FOREIGN KEY REFERENCES Suppliers(SupplierID),
-	EmployeeID INT FOREIGN KEY REFERENCES Employees(EmployeeID),
-	DeliveryDate DATE NOT NULL
-)
-
-CREATE TABLE PurchaseOrderDetails(
-	PurchaseOrderDetailID INT PRIMARY KEY IDENTITY NOT NULL,
-	PurchaseOrderID INT FOREIGN KEY REFERENCES PurchaseOrders(PurchaseOrderID),
-	ProductID INT FOREIGN KEY REFERENCES Products(ProductID),
-	Quantity INT NOT NULL,
-	Price NUMERIC NOT NULL
-)
-
-CREATE TABLE Address
-(
-	AddressID int PRIMARY KEY IDENTITY (1, 1),
-	AddressType int NULL,
-	Appartament varchar(10) NULL,
-	City varchar(30)  NULL,
-	Region varchar(25) NULL,
-	PostaCode int NULL
-)
-
 
 
 CREATE TABLE Employees (
-      EmployeID INT IDENTITY(1, 1) PRIMARY KEY
+      EmployeeID INT IDENTITY(1, 1) PRIMARY KEY
     , FirstName NVARCHAR(30) NOT NULL
     , LastName NVARCHAR(30) NOT NULL
     , Position NVARCHAR(30) NOT NULL
@@ -109,14 +104,16 @@ CREATE TABLE Orders
     DeliveryID int NOT NULL,
     TotalCost numeric (18,2) NULL,
     TotalDiscount numeric(18,2) NULL,
-    EmployeID int not NULL ,
+    EmployeeID int not NULL ,
     OrderData datetime NULL,
     InvoiceData datetime NULL,
     PaymentMethod nvarchar (100) NULL,
 
  FOREIGN KEY(CustomerID) REFERENCES Customers(CustomerID),
  FOREIGN KEY(DeliveryID) REFERENCES Deliveries(DeliveryID),
- FOREIGN KEY(EmployeID) REFERENCES Employees(EmployeID))
+ FOREIGN KEY(EmployeeID) REFERENCES Employees(EmployeeID))
+
+
 
  CREATE TABLE OrderDetails
  
@@ -129,12 +126,36 @@ CREATE TABLE Orders
 
   FOREIGN KEY(OrderID) REFERENCES Orders(OrderID),
   FOREIGN KEY(ProductID) REFERENCES Products(ProductID))
-  
-CREATE TABLE Deliveries(
-       DeliveryID INT PRIMARY KEY IDENTITY NOT NULL
-       , AddressID INT  FOREIGN KEY (AddressID) REFERENCES Address (AddressID)
-       , CompanyName NVARCHAR(50) NULL
-       , DepartureDate DATE NULL
+
+  CREATE TABLE Returns(
+       ReturnID INT PRIMARY KEY IDENTITY NOT NULL
+       , OrderDetailID int NULL
+       , Quantity int NULL
+       , Date date NULL
+       , Reason nvarchar(50) NULL
+       , Status nvarchar(30) NULL
+       FOREIGN KEY (OrderDetailID) REFERENCES OrderDetails (OrderDetailID)
 )
 
+
+CREATE TABLE PurchaseOrders(
+	PurchaseOrderID INT PRIMARY KEY IDENTITY NOT NULL,
+	SuplierID INT FOREIGN KEY REFERENCES Suppliers(SupplID),
+	EmployeeID INT FOREIGN KEY REFERENCES Employees(EmployeeID),
+	DeliveryDate DATE NOT NULL
+)
+
+
+CREATE TABLE PurchaseOrderDetails(
+	PurchaseOrderDetailID INT PRIMARY KEY IDENTITY NOT NULL,
+	PurchaseOrderID INT FOREIGN KEY REFERENCES PurchaseOrders(PurchaseOrderID),
+	ProductID INT FOREIGN KEY REFERENCES Products(ProductID),
+	Quantity INT NOT NULL,
+	Price NUMERIC NOT NULL
+)
+
+
+ ON [PRIMARY]
 GO
+
+
