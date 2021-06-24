932455b2f0661db838d820b8e63a4472104ba0b1
Mariana1808 <86299611+Mariana1808@users.noreply.github.com>
2021-06-24 07:17:03 -0300

    Create Shop-LV-608.sql

--- Shop-LV-608.sql
+++ Shop-LV-608.sql
@@ -12,21 +12,10 @@ CREATE TABLE Customers(
 )
 
 CREATE TABLE Returns(
-       Return_ID int Not Null Primary Key
-       , OrderDetail_ID int Not Null
+       ReturnID int Not Null Primary Key
+       , OrderDetailID int Not Null
        , ReturnQuantity int Not Null
        , ReturnDate date Not null
        , ReturnReason nvarchar(50) Null
        , ReturnStatus nvarchar(50) Not Null
 )
-
-CREATE TABLE Adress
-(
-	AddressID int PRIMARY KEY IDENTITY,
-	AdressType int NOT NULL,
-	Appartament Varchar(10) NOT NULL,
-	City Varchar(30) NOT NULL,
-	Region Varchar(25)NOT NULL,
-	PostaCode int NOT NULL
-)
-GO
