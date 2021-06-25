c0769335cce5a0dea0fb7a23793e1dcdd8142ed7
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-06-25 17:10:21 +0300

    Generated SQL script for PurchaseOrders

--- data_generator/csv_to_sql.py
+++ data_generator/csv_to_sql.py
@@ -11,16 +11,17 @@ csvFile = csv.reader(openInFile)
 header = next(csvFile)
 headers = map((lambda x: f"`{x}`"), header)
 headers_without_id = list(headers)[1:]
-insert = f"INSERT INTO {tableName} ({', '.join(headers_without_id)}) VALUES "
+insert = f"INSERT INTO {tableName} ({', '.join(headers_without_id)}) VALUES"
 
+openOutFile.write(insert + '\n')
 for row in csvFile:
     values = row[1:3]
     values.append(f'"{row[3]}"')
-    statement = insert + f"({', '.join(values)});"
-    print(statement)
-    openOutFile.write(statement + '\n')
     
-closing = ")"
+    val_statement = f"({', '.join(values)}),"
+    openOutFile.write(val_statement + '\n')
+    print(val_statement)
 
 openInFile.close()
 openOutFile.close()
+print(f"Done! Saved in {outputFilename}")
