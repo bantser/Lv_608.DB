c3bfa8b14cf1b411df524f143954de5e240c4448
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-06-25 18:11:06 +0300

    Devide SQL Insert statement by 1000 rows chunks

--- data_generator/csv_details_to_sql.py
+++ data_generator/csv_details_to_sql.py
@@ -3,23 +3,32 @@ import csv
 inputFilename = "PurchaseOrderDetails.csv"
 outputFilename = "PurchaseOrderDetails.sql"
 tableName = 'PurchaseOrderDetails'
+chunksize = 1000
 
 openInFile = open(inputFilename, 'r')
 openOutFile = open(outputFilename, 'w')
 
 csvFile = csv.reader(openInFile)
 header = next(csvFile)
-headers = map((lambda x: f"`{x}`"), header)
+headers = list(header)
 headers_without_id = list(headers)[1:]
 insert = f"INSERT INTO {tableName} ({', '.join(headers_without_id)}) VALUES"
 
 openOutFile.write(insert + '\n')
+cntr = 0
 for row in csvFile:
     values = row[1:]
+    if cntr < chunksize-1:
+        val_statement = f"({', '.join(values)}),"
+        openOutFile.write(val_statement + '\n')
     
-    val_statement = f"({', '.join(values)}),"
-    openOutFile.write(val_statement + '\n')
-    print(val_statement)
+        cntr += 1
+    else:
+        val_statement = f"({', '.join(values)});"
+        openOutFile.write(val_statement + '\n')
+        openOutFile.write(insert + '\n')
+        
+        cntr = 0
 
 openInFile.close()
 openOutFile.close()
