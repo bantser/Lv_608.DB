4c6515c2fe61e72dc2e1f67dd79d6d036b7a9046
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-06-25 16:57:10 +0300

    csv to sql

--- data_generator/csv_to_sql.py
+++ data_generator/csv_to_sql.py
@@ -1,17 +1,26 @@
 import csv
 
 inputFilename = "PurchaseOrders.csv"
+outputFilename = "PurchaseOrders.sql"
 tableName = 'PurchaseOrders'
 
 openInFile = open(inputFilename, 'r')
+openOutFile = open(outputFilename, 'w')
 
 csvFile = csv.reader(openInFile)
 header = next(csvFile)
 headers = map((lambda x: f"`{x}`"), header)
-insert = f"INSERT INTO {tableName} ({', '.join(headers)}) VALUES "
+headers_without_id = list(headers)[1:]
+insert = f"INSERT INTO {tableName} ({', '.join(headers_without_id)}) VALUES "
 
 for row in csvFile:
-    values = map((lambda x: f'"{x}"'), row)
-    print(insert + f"({', '.join(values)});")
+    values = row[1:3]
+    values.append(f'"{row[3]}"')
+    statement = insert + f"({', '.join(values)});"
+    print(statement)
+    openOutFile.write(statement + '\n')
+    
+closing = ")"
 
 openInFile.close()
+openOutFile.close()
